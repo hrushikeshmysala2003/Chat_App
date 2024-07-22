0d405d28ff9f55c57a32e6b438f76f2d1bbd0318
rushi <hrushikeshmysala2003@gmail.com>
2024-07-22 11:42:08 +0530

    Added CSS to join page

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Join.css";
 const Join = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
@@ -20,7 +21,7 @@ const Join = () => {
         <div>
           {" "}
           <input
-            className="joinInput"
+            className="joinInput mt-20"
             type="text"
             placeholder="Room"
             onChange={(e) => setRoom(e.target.value)}
@@ -30,7 +31,7 @@ const Join = () => {
           onClick={(event) => (!name || !room ? event.preventDefault() : null)}
           to={`/chat?name={${name}}&room=${room}`}
         >
-          <button className="button" type="submit">
+          <button className="button mt-20 " type="submit">
             Sign In
           </button>
         </Link>
