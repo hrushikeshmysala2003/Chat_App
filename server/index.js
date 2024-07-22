1456941fd1795dbc7664dd79800e76dd9090c349
rushi <hrushikeshmysala2003@gmail.com>
2024-07-22 10:36:42 +0530

    Added socket.io starter code and router middleware

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
-
+const router = require("./router");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server)
+const io = socketio(server);
 
+// All the socket code will be in this function because we are managing this socket connected
+io.on("connection", (socket) => {
+  console.log("We have a new connection");
+
+  socket.on("disconnect", () => {
+    console.log("User had left");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
